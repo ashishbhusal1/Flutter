90f9950ccf78057782f2e417acaa74e74b12eb27
ashishbhusal1 <ashishbhusal84@gmail.com>
2024-07-26 09:15:45 +0545

    small change

--- bloc/counter_app/lib/home_page.dart
+++ bloc/counter_app/lib/home_page.dart
@@ -1,5 +1,4 @@
 import "package:counter_app/bloc/counter_bloc.dart";
-import "package:counter_app/cubit/counter_cubit.dart";
 import "package:counter_app/inc_dec_page.dart";
 import "package:flutter/material.dart";
 import "package:flutter_bloc/flutter_bloc.dart";
@@ -11,8 +10,8 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final counterCubit = BlocProvider.of<CounterCubit>(context);
-    final counterBloc = BlocProvider.of<CounterBloc>(context);
+    //final counterCubit = BlocProvider.of<CounterCubit>(context);
+    //final counterBloc = BlocProvider.of<CounterBloc>(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
